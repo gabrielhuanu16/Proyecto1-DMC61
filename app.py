dac60f1708ccbe1c090006ec3bd9292e0502e33c
gabrielhuanu16 <gabrielhuanu16@gmail.com>
2026-08-14 16:36:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ import libreria_funciones as lf
 st.sidebar.title("Secciones")
 st.sidebar.image("DMC_logo.png", width=100)
 modulo = st.sidebar.selectbox("Seleccione un módulo", ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
+if modulo == "Home":
+  st.title("Modulo 1 - Especialización en Python for Analytics")
+  st.image("DMC_logo.png", width=300)
+  st.subheader("**Elaborado por**: Fabricio Gabriel Huánuco Rivero")
+  st.markdown("Carrera Profesional: Ingenieria de Sistemas e Informatica")
 
