4a814bbdf7dc6bdb4438dea619d157eb54cbdfd8
gabrielhuanu16 <gabrielhuanu16@gmail.com>
2026-08-14 16:37:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.sidebar.image("DMC_logo.png", width=100)
 modulo = st.sidebar.selectbox("Seleccione un módulo", ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
 if modulo == "Home":
   st.title("Modulo 1 - Especialización en Python for Analytics")
-  st.image("DMC_logo.png", width=300)
-  st.subheader("**Elaborado por**: Fabricio Gabriel Huánuco Rivero")
+  st.image("DMC_logo.png", width=200)
+  st.subheader("**Elaborado por:**"" Fabricio Gabriel Huánuco Rivero")
   st.markdown("Carrera Profesional: Ingenieria de Sistemas e Informatica")
 
