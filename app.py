86978cd751f72997667c5c79a74ae74a1b412166
gabrielhuanu16 <gabrielhuanu16@gmail.com>
2026-08-14 16:25:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+import streamlit as st
+import numpy as np
+import libreria_funciones as lf
 
+st.sidebar.title("Secciones")
+modulo = st.siderbar.selectbox("Seleccione un módulo", ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
