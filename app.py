2af30699e5d77f05742a0593d9c12c8b8433153a
gabrielhuanu16 <gabrielhuanu16@gmail.com>
2026-08-14 16:47:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,17 @@ if modulo == "Home":
   st.title("Modulo 1 - Especialización en Python for Analytics")
   st.image("DMC_logo.png", width=200)
   st.subheader("Elaborado por: Fabricio Gabriel Huánuco Rivero")
+  st.divider()
   st.markdown("Carrera Profesional: Ingenieria de Sistemas e Informatica")
   st.markdown("Universidad: Universidad Continental")
   st.markdown("Ciclo: 6to ciclo")  
   st.markdown("Especialización: Python for Analytics")
-  st.markdown("Año: 2026")
-
+  st.write("Año: 2026")
+  st.subheader("Este proyecto consiste en desarrollar una aplicación web utilizando Python y Streamlit, aplicando los fundamentos de programación aprendidos en el módulo Python Fundamentals. La aplicación busca presentar de manera interactiva la información y los resultados obtenidos durante el desarrollo del proyecto.")
+  st.markdown("""
+  - Python
+  - Streamlit
+  - GitHub
+  """)
   
 
