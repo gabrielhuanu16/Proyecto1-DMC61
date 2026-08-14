8ea70b380840704f0e1c5f6333ae2132c68cdb50
gabrielhuanu16 <gabrielhuanu16@gmail.com>
2026-08-14 16:58:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if modulo == "Home":
   st.write("**Descripción del Proyecto**")
   st.write("Este proyecto consiste en desarrollar una aplicación web utilizando Python y Streamlit, aplicando los fundamentos de programación aprendidos en el módulo Python Fundamentals. La aplicación busca presentar de manera interactiva la información y los resultados obtenidos durante el desarrollo del proyecto.")
   st.divider()
-  st.wirte("**Herramientas usadas**")
+  st.write("**Herramientas usadas**")
   st.markdown("""
   - Python
   - Streamlit
